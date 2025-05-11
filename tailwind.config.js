ccace72fdd770e08e79d48bc52ec3aa77e09b87e
RanaJyotika <srrana956@gmail.com>
2025-05-11 18:50:26 +0530

    Merging upstream changes and applying stashed changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 // tailwind.config.js
 module.exports = {
+   content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
   theme: {
     extend: {
+      animation: {
+        "scroll-right": "scrollRight 20s linear infinite",
+
+        "shimmer": "shimmer 2s linear infinite",
+      },
       keyframes: {
         scrollLeft: {
           "0%": { transform: "translateX(100%)" },
           "100%": { transform: "translateX(-100%)" },
+          
+        },
+         shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
         },
       },
       animation: {
